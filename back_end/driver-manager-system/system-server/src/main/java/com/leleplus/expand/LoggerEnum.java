cc2b42f36b38f387b218f2f783ce0c0204351ddd
lele <fgwang.660@gmail.com>
2020-01-15 23:33:58 +0800

    新增log4j2日志功能，添加多平台日志输出控制台和定向输出日志文件的功能，添加日志获取类，添加日志enum类，日志保存在%server_hone%/logs/,api模块和system模块，日志分开记录

---
+++ back_end/driver-manager-system/system-server/src/main/java/com/leleplus/expand/LoggerEnum.java
@@ -0,0 +1,31 @@
+package com.leleplus.expand;
+
+/**
+ * Copyright (C) @2020 fgwang.660@gmail.com
+ *
+ * @author lele
+ * @version 1.0
+ * @ClassName LoggerEnum
+ * @date 2020-01-15 22:19
+ * @description 日志相关的枚举类
+ */
+public enum LoggerEnum {
+    BUSINESS("business"),
+    PLATFORM("platform"),
+    DATABASES("databases"),
+    ERROR("error");
+    
+    private String code;
+    
+    LoggerEnum (String code){
+        this.code = code;
+    }
+    
+    public String getCode () {
+        return code;
+    }
+    
+    public void setCode (String code) {
+        this.code = code;
+    }
+}
