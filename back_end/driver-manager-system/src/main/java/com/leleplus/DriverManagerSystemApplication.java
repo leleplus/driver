f49e932615390eca8afb7aaf10b14c4f0c17f8e4
witt <fgwang.660@gmail.com>
2020-03-16 18:08:16 +0800

    Java服务端重构初始化

---
+++ back_end/driver-manager-system/src/main/java/com/leleplus/DriverManagerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.leleplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverManagerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverManagerSystemApplication.class, args);
+    }
+
+}
