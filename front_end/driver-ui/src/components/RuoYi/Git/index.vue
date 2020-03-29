666cf894f459e3f37dd8923a13c22d17b42554d9
witt <fgwang.660@gmail.com>
2020-03-29 16:13:58 +0800

    ui初始化

---
+++ front_end/driver-ui/src/components/RuoYi/Git/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <svg-icon icon-class="github" @click="goto"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'leleplusGitHub',
+  data() {
+    return {
+      url: 'https://github.com/leleplus/driver'
+    }
+  },
+  methods: {
+    goto() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
