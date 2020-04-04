9ae6de8cc62fb9e556eb3a0224e2d0b6187d92d6
witt <fgwang.660@gmail.com>
2020-04-04 23:39:02 +0800

    修改logo

--- front_end/driver-ui/src/layout/components/Sidebar/Logo.vue
+++ front_end/driver-ui/src/layout/components/Sidebar/Logo.vue
@@ -52,28 +52,30 @@ export default {
   text-align: center;
   overflow: hidden;
 
-  & .sidebar-logo-link {
-    height: 100%;
-    width: 100%;
+    & .sidebar-logo-link {
+        height: 100%;
+        width: 100%;
 
-    & .sidebar-logo {
-      width: 120px;
-      height: 70px;
-      vertical-align: middle;
-      margin-right: 12px;
-    }
+        // vue logo
+        & .sidebar-logo {
+            margin-top: -10px;
+            width: 100%;
+            /*height: 100px;*/
+            vertical-align: middle;
+            margin-right: 12px;
+        }
 
-    & .sidebar-title {
-      display: inline-block;
-      margin: 0;
-      color: #fff;
-      font-weight: 600;
-      line-height: 50px;
-      font-size: 14px;
-      font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
-      vertical-align: middle;
+        & .sidebar-title {
+            display: inline-block;
+            margin: 0;
+            color: #fff;
+            font-weight: 600;
+            line-height: 50px;
+            font-size: 14px;
+            font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
+            vertical-align: middle;
+        }
     }
-  }
 
   &.collapse {
     .sidebar-logo {
